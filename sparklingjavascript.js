8a530b2e75d004fcf973c786231e97be8773a33a
kryanosko <rosieposie@Rosies-MacBook-Air.local>
2016-11-07 10:06:25 -0800

    Proto 1
    Update CSS

---
+++ sparklingjavascript.js
@@ -0,0 +1,9 @@
+//INST630 Final Project-Stephani Moore/Rosie Yanosko
+//Sparkling Creations JavaScript Page
+
+//Need code for having already made jewelry pieces pop out in smaller window
+
+//Need code for having the person's custom ring photo to show up based
+//on form's input
+
+//Need code for adding items to the cart
